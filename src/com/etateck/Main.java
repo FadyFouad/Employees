d5b8cafacf517382ca1f9b17e6cb4603d3396cff
fadyfouad <fady.fouad.a@gmail.com>
2019-06-17 17:40:12 +0200

    [عىؤفهخى

--- src/com/etateck/Main.java
+++ src/com/etateck/Main.java
@@ -3,10 +3,9 @@ package com.etateck;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 /****************************************************
  *** Created by Fady Fouad on 6/16/2019 at 21:14.***
@@ -15,12 +14,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("\n-------------------------------Employees------------------------\n");
 
-        Employee employee3 = new Employee("Fady3", 27);
-        Employee employee1 = new Employee("Fady1", 23);
-        Employee employee2 = new Employee("Fady2", 25);
-        Employee employee4 = new Employee("Fady4", 31);
-        Employee employee5 = new Employee("Fady5", 35);
-
+        Employee employee3 = new Employee("Fady3 3_LastName", 27);
+        Employee employee1 = new Employee("Fady1 1_LastName", 23);
+        Employee employee2 = new Employee("Fady2 2_LastName", 25);
+        Employee employee4 = new Employee("Fady4 4_LastName", 31);
+        Employee employee5 = new Employee("Fady5 5_LastName", 35);
         List<Employee> employees = new ArrayList<>();
         employees.add(employee1);
         employees.add(employee2);
@@ -34,37 +32,19 @@ public class Main {
 
         IntPredicate intPredicate = value -> value>10;
         IntPredicate intPredicate1 = value -> value<20;
-
-        IntPredicate intPredicate_ = new IntPredicate() {
-            @Override
-            public boolean test(int value) {
-                return value>10;
-            }
-        };
-        IntPredicate intPredicate_1 = new IntPredicate() {
-            @Override
-            public boolean test(int value) {
-                return value<20;
-            }
-        };
-
         System.out.println("----------------------------------");
         System.out.println(intPredicate.and(intPredicate1).test(15));
-        System.out.println(intPredicate_.and(intPredicate_1).test(15));
 
-//        Predicate<Employee> employeePredicate = new Predicate<Employee>() {
-//            @Override
-//            public boolean test(Employee employee) {
-//                return employee.getAge()<27;
-//            }
-//        };
-//        printListLambda(employees,"-------------- age less than 27",employeePredicate);
-
-//        System.out.println("---------------------------------------------");
-//        Supplier<Integer>integerSupplier = ()-> new Random().nextInt(100);
-//        for (int i = 0; i < 10; i++) {
-//            System.out.println(integerSupplier.get());
-//        }
+//        int index = 0;
+        employees.forEach(employee->{
+            String[] name = employee.getName().split(" ");
+            String firstName=name[0];
+            String lastName = employee.getName().substring(employee.getName().indexOf(' ')+1);
+            Function<Employee,String>stringFunction = e ->
+                    e.getName().substring(e.getName().indexOf(' ')+1);
+            String s = stringFunction.apply(employees.get(employees.indexOf(employee)));
+            System.out.println("Last Name Of "+firstName +" is "+s);
+        });
     }
     private static void printListLambda(List<Employee>employees, String age, Predicate<Employee>ageCondition){
 
