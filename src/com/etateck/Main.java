22983ba24fd6474589f6af50462c5021df9c0621
fadyfouad <fady.fouad.a@gmail.com>
2019-06-18 21:15:36 +0200

    Function & FunctionChain

--- src/com/etateck/Main.java
+++ src/com/etateck/Main.java
@@ -3,9 +3,7 @@ package com.etateck;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.IntPredicate;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 /****************************************************
  *** Created by Fady Fouad on 6/16/2019 at 21:14.***
@@ -27,32 +25,53 @@ public class Main {
         employees.add(employee5);
 
         employees.sort(Comparator.comparing(Employee::getName));
-        printListLambda(employees,"=========> 30th",employee -> employee.getAge()>30);
-        printListLambda(employees,"=========> Less Than30",employee -> employee.getAge()<30);
+        printListLambda(employees, "=========> 30th", employee -> employee.getAge() > 30);
+        printListLambda(employees, "=========> Less Than30", employee -> employee.getAge() < 30);
 
-        IntPredicate intPredicate = value -> value>10;
-        IntPredicate intPredicate1 = value -> value<20;
+        IntPredicate intPredicate = value -> value > 10;
+        IntPredicate intPredicate1 = value -> value < 20;
         System.out.println("----------------------------------");
         System.out.println(intPredicate.and(intPredicate1).test(15));
 
 //        int index = 0;
-        employees.forEach(employee->{
+        employees.forEach(employee -> {
             String[] name = employee.getName().split(" ");
-            String firstName=name[0];
-            String lastName = employee.getName().substring(employee.getName().indexOf(' ')+1);
-            Function<Employee,String>stringFunction = e ->
-                    e.getName().substring(e.getName().indexOf(' ')+1);
+            String firstName = name[0];
+            String lastName = employee.getName().substring(employee.getName().indexOf(' ') + 1);
+            Function<Employee, String> stringFunction = e ->
+                    e.getName().substring(e.getName().indexOf(' ') + 1);
             String s = stringFunction.apply(employees.get(employees.indexOf(employee)));
-            System.out.println("Last Name Of "+firstName +" is "+s);//s = lastName
+            System.out.println("Last Name Of " + firstName + " is " + s);//s = lastName
         });
-    }
-    private static void printListLambda(List<Employee>employees, String age, Predicate<Employee>ageCondition){
+        System.out.println("*****************************************************");
 
-            System.out.println(age);
-            employees.forEach(employee -> {
-                if (ageCondition.test(employee))
-                    System.out.println(employee.getName());
-            });
+        Function<Employee, String> upperCase = employee -> employee.getName().toUpperCase();
+        Function<String, String> getFirstName = name -> name.substring(0, name.indexOf(' '));
+        Function<String, String> getLastName = name -> name.substring(name.indexOf(' ')+1);
+        Function chainFunction = upperCase.andThen(getFirstName);
+
+
+        System.out.println(chainFunction.apply(employees.get(0)));
+
+        BiFunction<String, Employee, String> getUpperAge = (name, employee) -> name.concat(" " + employee.getAge());
+        String upperName = upperCase.apply(employees.get(0));
+        System.out.println(getUpperAge.apply(upperName,employee1));
+
+        IntUnaryOperator incByFive = i->i+5;
+        System.out.println(incByFive.applyAsInt(15));
+
+        Consumer<String>consumer1 = s-> s.toUpperCase();
+        Consumer<String>consumer2 = s-> System.out.println(s);
+        consumer1.andThen(consumer2).accept("Hello");//No consumer1
+    }
+
+    private static void printListLambda(List<Employee> employees, String age, Predicate<Employee> ageCondition) {
+
+        System.out.println(age);
+        employees.forEach(employee -> {
+            if (ageCondition.test(employee))
+                System.out.println(employee.getName());
+        });
 
     }
 }
